f023e6ff8a8db6335fd921d44844daddfcf03c08
Vidhi Zala <vidhi@helpshift.com>
2014-04-11 18:21:36 +0530

    Improve Adapter implementation for rendering listview

--- src/com/vidhi/sodemoapp/CustomListViewAdapter.java
+++ src/com/vidhi/sodemoapp/CustomListViewAdapter.java
@@ -1,4 +1,5 @@
-package com.vidhi.sodemoapp; /**
+package com.vidhi.sodemoapp;
+/**
  * Created by vidhi on 3/24/14.
  */
 
@@ -26,71 +27,68 @@ public class CustomListViewAdapter extends ArrayAdapter<QuestionInfo> {
 
     /**
      * Function used by ArrayAdapter to iterate through the list of questionInfo objects and render each in the single_list_element layout
-     *
-     * @param paramInt       - used to access the current element from the list of questionInfo objects
-     * @param paramView      - single_list_element layout
-     * @param paramViewGroup
+     * @param position       - used to access the current element from the list of questionInfo objects
+     * @param convertView      - single_list_element layout
+     * @param parent
      * @return listview with rendered data of questionInfo objects
      */
     @Override
-    public View getView(int paramInt, View paramView, ViewGroup paramViewGroup) {
-        QuestionInfo qInfo;
-        // assign the view we are converting to a local variable
-        View v = paramView;
+    public View getView(int position, View convertView, ViewGroup parent) {
+        QuestionInfo qInfo = getItem(position);
+        ViewHolder holder = null;
         // first check to see if the view is null. if so, we have to inflate it.
-        if (v == null) {
+        if (convertView == null) {
+            holder = new ViewHolder();
             LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            v = inflater.inflate(R.layout.single_list_element, null);
+            convertView = inflater.inflate(R.layout.single_list_element, null);
+            convertView.setTag(holder);
+        }else{
+            holder = (ViewHolder) convertView.getTag();
         }
 
-        /*
-         * Recall that the variable position is sent in as an argument to this method.
-		 * The variable simply refers to the position of the current object in the list. (The ArrayAdapter
-		 * iterates through the list we sent it)
-		 *
-		 * Therefore, paramInt refers to the current Item object.
-		 */
-        qInfo = (QuestionInfo) getItem(paramInt);
+        holder.setViews(convertView);
+        holder.tQuestionTitle.setText(qInfo.getQuestion());
+        holder.tScore.setText(qInfo.getScore());
+        String[] tempQuestionTags = qInfo.getTags();
+        holder.setQuestionTags(tempQuestionTags);
 
-        if (qInfo != null) {
-
-            TextView tQuestionTitle = (TextView) v.findViewById(R.id.questiontitle);
-            TextView tTag1 = (TextView) v.findViewById(R.id.tag1);
-            TextView tTag2 = (TextView) v.findViewById(R.id.tag2);
-            TextView tTag3 = (TextView) v.findViewById(R.id.tag3);
-            TextView tTag4 = (TextView) v.findViewById(R.id.tag4);
-            TextView tTag5 = (TextView) v.findViewById(R.id.tag5);
-            TextView tScore = (TextView) v.findViewById(R.id.questionscore);
-
-            // check to see if each individual textview is null.
-            // if not, assign some text
-            if (tQuestionTitle != null) {
-                tQuestionTitle.setText(qInfo.getQuestion());
-            }
-
-            String[] tempTags = qInfo.getTags();
-
-            if (tTag1 != null && tempTags[0] != null) {
-                tTag1.setText(tempTags[0]);
-            }
-            if (tTag2 != null && tempTags[1] != null) {
-                tTag2.setText(tempTags[1]);
-            }
-            if (tTag3 != null && tempTags[2] != null) {
-                tTag3.setText(tempTags[2]);
-            }
-            if (tTag4 != null && tempTags[3] != null) {
-                tTag4.setText(tempTags[3]);
-            }
-            if (tTag5 != null && tempTags[4] != null) {
-                tTag5.setText(tempTags[4]);
-            }
-            if (tScore != null) {
-                tScore.setText(qInfo.getScore());
-            }
-        }
         // the view must be returned to our activity
-        return v;
+        return convertView;
+    }
+
+    private static class ViewHolder{
+        public TextView tQuestionTitle;
+        public TextView tTag1;
+        public TextView tTag2;
+        public TextView tTag3;
+        public TextView tTag4;
+        public TextView tTag5;
+        public TextView tScore;
+
+        public void setQuestionTags(String tempTags[]){
+            if(tempTags[0] != null)
+                tTag1.setText(tempTags[0]);
+            if(tempTags[1] != null)
+                tTag2.setText(tempTags[1]);
+            if(tempTags[2] != null)
+                tTag3.setText(tempTags[2]);
+            if(tempTags[3] != null)
+                tTag4.setText(tempTags[3]);
+            if(tempTags[4] != null)
+                tTag5.setText(tempTags[4]);
+
+        }
+
+        public void setViews(View convertView){
+            tQuestionTitle = (TextView) convertView.findViewById(R.id.questiontitle);
+            tTag1 = (TextView) convertView.findViewById(R.id.tag1);
+            tTag2 = (TextView) convertView.findViewById(R.id.tag2);
+            tTag3 = (TextView) convertView.findViewById(R.id.tag3);
+            tTag4 = (TextView) convertView.findViewById(R.id.tag4);
+            tTag5 = (TextView) convertView.findViewById(R.id.tag5);
+            tScore = (TextView) convertView.findViewById(R.id.questionscore);
+
+        }
     }
 
 }
